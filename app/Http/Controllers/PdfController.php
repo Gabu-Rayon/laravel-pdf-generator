21e6541c4e9f569ae7e823868baf1adca2dad923
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-04-16 01:04:39 +0300

    new changes

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\App;
+  use Barryvdh\DomPDF\Facade\Pdf;
 class PdfController extends Controller
 {
- public function generatePdf(){
-    return view();
+ public function generatePdf(){     
+    
+    $pdf = App::make('dompdf.wrapper');
+    $pdf->loadHTML('<h1>Hello Wolrd Test</h1>');
+    return $pdf->stream();
  }
 }
