8d132624042cbaf7a56fba2a29c0a3b99502fbe2
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-04-16 00:53:06 +0300

    commit changes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PdfController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('generate-pdf', [PdfController::class,'generatePdf']) ->name('generate-pdf');
