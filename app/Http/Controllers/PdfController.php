50cd4f0867795d6be7ee758c475c0930d47c1b77
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-04-16 12:41:10 +0300

    new changes

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -7,16 +7,26 @@ use Illuminate\Support\Facades\App;
   use Barryvdh\DomPDF\Facade\Pdf;
 class PdfController extends Controller
 {
+    
 //  public function generatePdf(){     
     
 //     $pdf = App::make('dompdf.wrapper');
 //     $pdf->loadHTML('<h1>Hello World Test</h1>');
 //     return $pdf->stream();
 //  }
-
-  public function generatePdf(){       
+    
+/****Generate from a Blade */
+//   public function generatePdf(){       
    
-    $pdf = Pdf::loadView('index');
+//     $pdf = Pdf::loadView('index');
+//     return $pdf->download();
+//  }
+
+ /*****Generate from a blade  which has been passed a variable */
+  public function generatePdf(){       
+    $data ="Hiae,My name is Gibson M Developer of this Repo !";
+    $pdf = Pdf::loadView('index', compact('data'));
     return $pdf->download();
  }
+
 }
