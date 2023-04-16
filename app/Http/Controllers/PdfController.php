4eab5e5ae77a5c489a7eda9eadde05faff281022
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-04-16 13:34:30 +0300

    new changes

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Str;
   use Barryvdh\DomPDF\Facade\Pdf;
 class PdfController extends Controller
 {
@@ -44,6 +44,7 @@ class PdfController extends Controller
 //     $pdf = Pdf::loadView('index', ['data' =>$data,'email'=>$email]);
 //     return $pdf->download();
 //  }
+
  /*****
   * Generate from a blade  which has been passed a variable
   *With a  Unique Name of Document */
@@ -54,5 +55,19 @@ class PdfController extends Controller
     $pdf = Pdf::loadView('index', ['data' =>$data,'email'=>$email]);
     return $pdf->download('my-file' .time(). rand('9999','99999'). Str::random('10').'.pdf');
  }
+
+
+
+  /*****
+  * Generate from a blade  which has been passed a variable
+  *With a  Unique Name of Document and Directory to save the doc*/
+  
+//     public function generatePdf(){       
+//     $data ="Hae,My name is Gibson M Developer of this Repo !";
+//     $email = "gibsonmurimi4@gmail.com";
+//     $pdf = Pdf::loadView('index', ['data' =>$data,'email'=>$email])->save(public_path('my-file' .time(). rand('9999','99999'). Str::random('10').'.pdf'));
+//     return $pdf->download();
+//  }
+
 
 }
